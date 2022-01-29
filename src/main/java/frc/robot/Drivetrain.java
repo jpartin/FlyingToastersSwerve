99dfdf161c7fee0a72af814dc3cccfe236962eae
Jim Partin <jpartin2@gmail.com>
2022-01-29 14:09:21 -0500

    Initial.

---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,93 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.ctre.phoenix.sensors.WPI_PigeonIMU;
+
+/** Represents a swerve drive style drivetrain. */
+public class Drivetrain {
+  public static final double kMaxSpeed = 3.0; // 3 meters per second
+  public static final double kMaxAngularSpeed = Math.PI; // 1/2 rotation per second
+
+  //TODO: Measure Module locations
+  private final Translation2d m_frontLeftLocation = new Translation2d(0.3937, 0.3937);
+  private final Translation2d m_frontRightLocation = new Translation2d(0.3937, -0.3937);
+  private final Translation2d m_backLeftLocation = new Translation2d(-0.3937, 0.3937);
+  private final Translation2d m_backRightLocation = new Translation2d(-0.3937, -0.3937);
+
+  //TODO: Ensure the motor IDs are set correctly.
+  private final SwerveModule m_frontLeft = new SwerveModule(1, 2);
+  private final SwerveModule m_frontRight = new SwerveModule(3, 4);
+  private final SwerveModule m_backLeft = new SwerveModule(5, 6);
+  private final SwerveModule m_backRight = new SwerveModule(7, 8);
+
+  private final WPI_PigeonIMU m_gyro = new WPI_PigeonIMU(0);
+
+  private final SwerveDriveKinematics m_kinematics =
+      new SwerveDriveKinematics(
+          m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+
+  private final SwerveDriveOdometry m_odometry =
+      new SwerveDriveOdometry(m_kinematics, m_gyro.getRotation2d());
+
+  public Drivetrain() {
+    m_gyro.reset();
+  }
+
+  /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param xSpeed Speed of the robot in the x direction (forward).
+   * @param ySpeed Speed of the robot in the y direction (sideways).
+   * @param rot Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+   */
+  @SuppressWarnings("ParameterName")
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+    var swerveModuleStates =
+        m_kinematics.toSwerveModuleStates(
+            fieldRelative
+                ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, m_gyro.getRotation2d())
+                : new ChassisSpeeds(xSpeed, ySpeed, rot));
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
+    m_frontLeft.setDesiredState(swerveModuleStates[0]);
+    m_frontRight.setDesiredState(swerveModuleStates[1]);
+    m_backLeft.setDesiredState(swerveModuleStates[2]);
+    m_backRight.setDesiredState(swerveModuleStates[3]);
+  }
+
+  /** Updates the field relative position of the robot. */
+  public void updateOdometry() {
+    m_odometry.update(
+        m_gyro.getRotation2d(),
+        m_frontLeft.getState(),
+        m_frontRight.getState(),
+        m_backLeft.getState(),
+        m_backRight.getState());
+  }
+
+  public void updateDashboard() {
+    SmartDashboard.putNumber("Front Left Module Angle", m_frontLeft.getState().angle.getDegrees());
+    SmartDashboard.putNumber("Front Right Module Angle", m_frontRight.getState().angle.getDegrees());
+    SmartDashboard.putNumber("Back Left Module Angle", m_backLeft.getState().angle.getDegrees());
+    SmartDashboard.putNumber("Back Right Module Angle", m_backRight.getState().angle.getDegrees());
+
+    SmartDashboard.putNumber("X Position", m_odometry.getPoseMeters().getX());
+    SmartDashboard.putNumber("Y Position", m_odometry.getPoseMeters().getY());
+
+    SmartDashboard.putNumber("Gyroscope Angle", m_gyro.getAngle());
+
+    // SmartDashboard.putNumber("Front Right Module Angle", Math.toDegrees(m_frontRight.getCurrentAngle()));
+    // SmartDashboard.putNumber("Back Left Module Angle", Math.toDegrees(backLeftModule.getCurrentAngle()));
+    // SmartDashboard.putNumber("Back Right Module Angle", Math.toDegrees(backRightModule.getCurrentAngle()));
+
+}
+}
